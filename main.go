7a62050fb628d393ca7da770e160311e5ba618b1
Aleksandr Sapozhnikov <alexxandrsap@mail.ru>
2022-08-21 20:45:13 +0300

    add main. add Move snake method

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	snk := NewSnake(Point{5, 5})
+	snk.Move(Up)
+
+}
--- snake.go
+++ snake.go
@@ -1,8 +1,17 @@
-package snake
+package main
 
 type Point struct {
 	X, Y uint8
 }
+
+type Direction uint8
+
+const (
+	Up Direction = iota
+	Down
+	Left
+	Right
+)
 
 type Snake struct {
 	body []Point
@@ -16,6 +25,31 @@ func (snk Snake) Eat(food Point) {
 	snk.body = append([]Point{food}, snk.body...)
 }
 
+func (snk Snake) Head() Point {
+	return snk.body[0]
+}
+
 func (snk Snake) Len() int {
 	return len(snk.body)
+}
+
+func (snk Snake) Move(dir Direction) {
+	prev := snk.Head()
+	snk.body[0] = func(dir Direction, prev Point) Point {
+		switch dir {
+		case Up:
+			return Point{prev.X, prev.Y - 1}
+		case Down:
+			return Point{prev.X, prev.Y + 1}
+		case Right:
+			return Point{prev.X + 1, prev.Y}
+		case Left:
+			return Point{prev.X - 1, prev.Y}
+		}
+		return prev
+	}(dir, prev)
+
+	for i := 1; i < snk.Len(); i++ {
+		snk.body[i], prev = prev, snk.body[i]
+	}
 }
