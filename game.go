8bf3cc800d5d3f7943a75efc5ba71dfc1fc65936
Aleksandr Sapozhnikov <alexxandrsap@mail.ru>
2022-08-23 17:30:24 +0300

    import SiDCo. Add drawBoxes method

--- game.go
+++ game.go
@@ -1,12 +1,26 @@
 package main
 
+import sdc "github.com/AlexxSap/SiDCo"
+
 type Game struct {
+	snakeField sdc.Canvas
+	dataField  sdc.Canvas
 }
 
 func NewGame() Game {
-	return Game{}
+	snake, _ := sdc.NewCanvas(sdc.Point{1, 1}, sdc.Point{20, 30})
+	data, _ := sdc.NewCanvas(sdc.Point{1, 35}, sdc.Point{10, 20})
+	return Game{
+		snakeField: snake,
+		dataField:  data,
+	}
 }
 
-func (game Game) Start() {
+func (gm Game) drawBoxes() {
+	gm.snakeField.DrawBoxWithTitle("SNAKE GAME")
+	gm.dataField.DrawBoxWithTitle("SCORE")
+}
 
+func (gm Game) Start() {
+	gm.drawBoxes()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module snake
 
 go 1.19
+
+require github.com/AlexxSap/SiDCo v0.0.0-20220820082216-6f96f5e1408e // indirect
